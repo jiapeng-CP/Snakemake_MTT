71b7e03bfed481e6e9eba3acb34eaca17e9291c4
jiapeng <jiapengc@alustt132.cm.cluster>
2024-01-24 15:23:03 -0600

    updated sumMetrics.py to update headers with more sense

--- sumMetrics.py
+++ sumMetrics.py
@@ -3,28 +3,32 @@ import json
 
 metaDF = pd.read_csv("samplesheet.tsv", sep = "\t", index_col="Sample")
 
-df = pd.DataFrame(columns=["Sample", "total_reads","totalreads4map","mappedReads","map_perc"])
+df = pd.DataFrame(columns=["Sample", "raw_reads","clean_reads","mapped2HOMD","mapped_perc"])
 for s in metaDF.index:
 
     # read fastp json
     fpjs = "fastp/" + s + ".json"
     f = open(fpjs)
     fpd = json.load(f)
+    raw_reads = fpd['summary']['before_filtering']['total_reads']
 
     # read map2HOMD json
     hojs = "map2HOMD/" + s + ".json"
     f = open(hojs)
     hopd = json.load(f)
-    map_perc = hopd['general']['reads']['mapped']['0']*100/hopd['general']['reads']['total']
+    clean_reads = hopd['general']['reads']['total'] # reads used for eHOMD mapping are trimmed, rRNA and human reads removed
+    mapped2HOMD = hopd['general']['reads']['mapped']['0']
+
+    mapped_perc = mapped2HOMD * 100 / clean_reads
 
 
     mlist = [s,
-              str(fpd['summary']['before_filtering']['total_reads']),
-              str(hopd['general']['reads']['total']),
-              str(hopd['general']['reads']['mapped']['0']),
-              str(map_perc)
+              str(raw_reads),
+              str(clean_reads),
+              str(mapped2HOMD),
+              str(mapped_perc)
               ]
-    result = "\t".join(mlist)
+
     df = df.append(pd.Series(mlist, index=df.columns), ignore_index=True)
 
 
